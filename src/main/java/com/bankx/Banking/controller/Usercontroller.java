5122c9c2de9d8f08669a34a74b8d712f7cf1b519
satishkoyya <satishbabukoyya@gmai.com>
2024-01-13 12:38:55 +0530

    first commit

---
+++ src/main/java/com/bankx/Banking/controller/Usercontroller.java
@@ -0,0 +1,42 @@
+package com.bankx.Banking.controller;
+
+import com.bankx.Banking.dto.BankResponse;
+import com.bankx.Banking.dto.CreditDebitRequest;
+import com.bankx.Banking.dto.EnquiryRequest;
+import com.bankx.Banking.dto.UserRequest;
+import com.bankx.Banking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public BankResponse createAccount(@RequestBody UserRequest userRequest){
+        return userService.createAccount(userRequest);
+    }
+
+    @GetMapping("balanceEnquiry")
+    public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request){
+        return userService.balanceEnquiry(request);
+    }
+
+    @GetMapping("nameEnquiry")
+    public String nameEnquiry(@RequestBody EnquiryRequest request){
+        return userService.nameEnquiry(request);
+    }
+
+    @PostMapping("credit")
+    public BankResponse creditAccount(@RequestBody CreditDebitRequest request){
+        return userService.creditAccount(request);
+    }
+
+    @PostMapping("debit")
+    public BankResponse debitAccount(@RequestBody CreditDebitRequest request){
+        return userService.debitAccount(request);
+    }
+}
---
+++ src/main/java/com/bankx/Banking/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bankx.Banking.repository;
+
+import com.bankx.Banking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
+    Boolean existsByAccountNumber(String accountNumber);
+    User findByAccountNumber(String accountNumber);
+}
